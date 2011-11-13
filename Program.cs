322ec0730844e335aeadde4023456b1716c7dc4f
Kyle Maher <p07r0457@gmail.com>
2011-11-13 10:23:52 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace Meep_Meep
+{
+    class Program
+    {
+
+        /// <summary>
+        /// Main entry point for program
+        /// </summary>
+        /// <param name="args">We ignore all arguments</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Meep meep!");
+        }
+    }
+}
